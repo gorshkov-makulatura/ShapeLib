671449c29e6a8c60e8000ee8a6fb34bfc8c4ae69
gorshkov-makulatura <gorshkov.makulatura@gmail.com>
2022-11-16 20:23:05 +0300

    Добавьте файлы проекта.

---
+++ ShapeLib/Shape.cs
@@ -0,0 +1,118 @@
+﻿namespace ShapeLib
+{
+    /// <summary>
+    /// Defines basic shape with Area.
+    /// </summary>
+    public abstract class Shape
+    {
+        /// <summary>
+        /// Gets area of the shape
+        /// </summary>
+        public abstract float Area { get; }
+    }
+
+    /// <summary>
+    /// Defines circle shape.
+    /// </summary>
+    public class Circle : Shape
+    {
+        public override float Area => MathF.Pow(Radius, 2) * MathF.PI;
+
+        /// <summary>
+        /// Gets radius of the circle.
+        /// </summary>
+        public float Radius { get; }
+
+        /// <summary>
+        /// Creates new circle with given radius.
+        /// </summary>
+        /// <param name="radius">Radius of the circle.</param>
+        public Circle(float radius)
+        {
+            Radius = radius;
+        }
+    }
+
+    /// <summary>
+    /// Defines triangle shape.
+    /// </summary>
+    public class Triangle : Shape
+    {
+        public override float Area { get; }
+
+        /// <summary>
+        /// Gets a value indicating whether this triangle has right angle.
+        /// </summary>
+        public bool IsRight { get; }
+
+        /// <summary>
+        /// Gets length of the longest triangle side.
+        /// </summary>
+        public float Hypotenuse { get; }
+
+        /// <summary>
+        /// Gets length of triangle catheus.
+        /// </summary>
+        public float CatheusL { get; }
+
+        /// <summary>
+        /// Gets length of triangle catheus.
+        /// </summary>
+        public float CatheusR { get; }
+
+        /// <summary>
+        /// Gets length of side A.
+        /// </summary>
+        public float A { get; }
+
+        /// <summary>
+        /// Gets length of side B.
+        /// </summary>
+        public float B { get; }
+
+        /// <summary>
+        /// Gets length of side C.
+        /// </summary>
+        public float C { get; }
+
+        /// <summary>
+        /// Creates a new triangle with specified sides.
+        /// </summary>
+        /// <param name="a">Length of side A.</param>
+        /// <param name="b">Length of side B.</param>
+        /// <param name="c">Length of side C.</param>
+        public Triangle(float a, float b, float c)
+        {
+            A = a;
+            B = b;
+            C = c;
+
+            if (A > B && A > C)
+            {
+                Hypotenuse = A;
+                CatheusL = B;
+                CatheusR = C;
+            }
+
+            if (B > A && B > C)
+            {
+                Hypotenuse = B;
+                CatheusL = A;
+                CatheusR = C;
+            }
+
+            if (C > A && C > B)
+            {
+                Hypotenuse = C;
+                CatheusL = A;
+                CatheusR = B;
+            }
+
+            // Heron's formula
+            float p = (A + B + C) * 0.5f;
+            Area = MathF.Sqrt(p * (p - A) * (p - B) * (p - C));
+
+            IsRight = MathF.Pow(CatheusL, 2) + MathF.Pow(CatheusR, 2) == MathF.Pow(Hypotenuse, 2);
+        }
+    }
+}
---
+++ ShapeLib_Tests/Shape_Tests.cs
@@ -0,0 +1,93 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using ShapeLib;
+
+namespace ShapeLib_Tests
+{
+    [TestClass]
+    public class Shape_Tests
+    {
+        [TestMethod]
+        public void Test_UnitCircleArea_EqualsPi()
+        {
+            // Arrange
+            Circle circle = new(1);
+
+            // Act
+            float actualArea = circle.Area;
+            float expectedArea = MathF.PI;
+
+            // Assert
+            Assert.AreEqual(expectedArea, actualArea);
+        }
+
+        [TestMethod]
+        public void Test_CircleAreaWithRadius5_Equals25Pi()
+        {
+            // Arrange
+            Circle circle = new(5);
+
+            // Act
+            float actualArea = circle.Area;
+            float expectedArea = 25 * MathF.PI;
+
+            // Assert
+            Assert.AreEqual(expectedArea, actualArea);
+        }
+
+        [TestMethod]
+        public void Test_EgyptTriangleArea_Equals6()
+        {
+            // Arrange
+            Triangle tri = new(5, 4, 3);
+
+            // Act
+            float actualArea = tri.Area;
+            float expectedArea = 6;
+
+            // Assert
+            Assert.AreEqual(expectedArea, actualArea);
+        }
+
+        [TestMethod]
+        public void Test_HypotenuseIsTheLongestsideOfTriangle()
+        {
+            // Arrange
+            Triangle tri = new(3, 5, 4);
+
+            // Act
+            float actual = tri.Hypotenuse;
+            float expected = 5;
+
+            // Assert
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod]
+        public void Test_IsRightTriangle_EqualsTrue()
+        {
+            // Arrange
+            Triangle tri = new(5, 4, 3);
+
+            // Act
+            bool actual = tri.IsRight;
+            bool expected = true;
+
+            // Assert
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod]
+        public void Test_IsRightTriangle_EqualsFalse()
+        {
+            // Arrange
+            Triangle tri = new(6, 4, 3);
+
+            // Act
+            bool actual = tri.IsRight;
+            bool expected = false;
+
+            // Assert
+            Assert.AreEqual(expected, actual);
+        }
+    }
+}
